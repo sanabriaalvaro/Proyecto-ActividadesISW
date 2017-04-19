85bba1dc25fba04d3ecfff5766c88a50c8f04e75
Alvaro Sanabria <sanabriaalvaro38@gmail.com>
2017-04-19 14:09:37 -0400

    Subiendo interfaces navegables

---
+++ Proyecto AISW/src/proyecto/aisw/ProyectoAISW.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.aisw;
+
+/**
+ *
+ * @author lcog86
+ */
+public class ProyectoAISW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Inicio I_inicio = new Inicio();
+        I_inicio.setLocationRelativeTo(null);   // Ubicar para centrar gui en la pantalla
+        I_inicio.setVisible(true);  
+        
+    }
+    
+}
